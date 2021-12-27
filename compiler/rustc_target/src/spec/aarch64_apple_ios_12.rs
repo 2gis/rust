34ac8a77cf6f61715f6452e277e2517ce477e2dd
Sameri11 <mozilamixa@gmail.com>
2021-12-27 23:42:30 +0700

    update target config

--- compiler/rustc_target/src/spec/aarch64_apple_ios_12.rs
+++ compiler/rustc_target/src/spec/aarch64_apple_ios_12.rs
@@ -10,9 +10,7 @@ pub fn target() -> Target {
         arch: "aarch64".to_string(),
         options: TargetOptions {
             features: "+neon,+fp-armv8,+apple-a7".to_string(),
-            eliminate_frame_pointer: false,
             max_atomic_width: Some(128),
-            unsupported_abis: super::arm_base::unsupported_abis(),
             forces_embed_bitcode: true,
             // Taken from a clang build on Xcode 11.4.1.
             // These arguments are not actually invoked - they just have
